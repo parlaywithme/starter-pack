14c36e368a7acb208bde7335253f84d516031b95
Loren Sands-Ramshaw <lorensr@gmail.com>
2015-02-25 23:51:29 -0800

    versions

--- package.js
+++ package.js
@@ -7,17 +7,17 @@ Package.describe({
 });
 
 Package.onUse(function(api) {
-  api.versionsFrom('1.0.0');
+  api.versionsFrom('1.0.3.2');
 
-  api.imply('iron:router');
-  api.imply('zimme:iron-router-auth');
-  api.imply('aldeed:collection2');
-  api.imply('zimme:collection-timestampable');
-  api.imply('useraccounts:core');
-  api.imply('aldeed:autoform');
-  api.imply('email');
-  api.imply('http');
-  api.imply('momentjs:moment');
+  api.imply('iron:router@1.0.7');
+  api.imply('zimme:iron-router-auth@3.0.0');
+  api.imply('aldeed:collection2@2.3.2');
+  api.imply('zimme:collection-timestampable@1.0.4');
+  api.imply('useraccounts:core@1.8.1');
+  api.imply('aldeed:autoform@4.2.2');
+  api.imply('email@1.0.5');
+  api.imply('http@1.0.10');
+  api.imply('momentjs:moment@2.9.0');
 
   // api.addFiles('parlay:starter-pack.js');
 });
